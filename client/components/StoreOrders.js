4e22934e0435aa97dac6f2f47c1d7a75ba7c406f
Jared Murphy <jaredcmurphy@gmail.com>
2017-07-07 11:48:19 -0400

    display orders working : )

--- client/components/StoreOrders.js
+++ client/components/StoreOrders.js
@@ -1,23 +1,58 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 const StoreOrders = (props) => {
   const { currentUser, currentStore, openOrders } = props;
-  console.log(openOrders)
-  const renderOrderItems = () => {
+  console.log(openOrders);
+
+  const formatDueDate = (dueDate, late) => {
+    const additionalString = late ? " ago" : " to go";
+    return (moment(dueDate).fromNow().split("in ")[1] + additionalString).toUpperCase();
+  }
+
+  const getOrderStatus = (order) => {
+    if (!order.due_date){
+      return {status: 'In Transit', color: 'green'};
+    } else if (order.late){
+      let dueTime = formatDueDate(order.due_date, true);
+      return {status: dueTime, color: 'red'};
+    } else {
+      let dueTime = formatDueDate(order.due_date, false);
+      return {status: dueTime, color: 'orange'};
+    } 
+  }
+
+  const renderOrderRows = () => {
     return openOrders.map((order, i) => {
+      const orderStatus = getOrderStatus(order);
       return (
-        <li>Order Item</li>
+        <tr key={i}>
+          <td>{order.id}</td>
+          <td style={{color: orderStatus.color}}>{orderStatus.status}</td>
+          <td>{order.customer_id}</td>
+          <td>Quantity</td>
+        </tr>
       );
     });
-
   }
+
   return (
     <div>
       <h3>Orders</h3>
-      <ul>
-        { renderOrderItems() }
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Order</th>
+            <th>Status</th>
+            <th>Customer</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          { renderOrderRows() }
+        </tbody>
+      </table>
     </div>
 
 
