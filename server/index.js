b9ce93fd3cd9470e2e4a78164021df2d47800a2c
Jared Murphy <jaredcmurphy@gmail.com>
2017-08-31 14:07:46 -0400

    removed webpack from server.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackConfig = require('../webpack.config.js');
+// const webpack = require('webpack');
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpackHotMiddleware = require('webpack-hot-middleware');
+// const webpackConfig = require('../webpack.config.js');
+//
+//
+// const compiler = webpack(webpackConfig);
+//
+// app.use(webpackMiddleware(compiler, {
+//   hot: true,
+//   publicPath: webpackConfig.output.publicPath,
+//   noInfo: true
+// }));
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const compiler = webpack(webpackConfig);
-
-app.use(webpackMiddleware(compiler, {
-  hot: true,
-  publicPath: webpackConfig.output.publicPath,
-  noInfo: true
-}));
-
 app.use(webpackHotMiddleware(compiler));
 app.use(bodyParser.json({limit: '1000mb'}));
 app.use(express.static('build'));
