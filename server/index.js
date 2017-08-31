4657d450d0dba50b0667d603468129d98e08e0b9
Jared Murphy <jaredcmurphy@gmail.com>
2017-08-31 14:10:36 -0400

    fuck

--- server/index.js
+++ server/index.js
@@ -13,10 +13,11 @@ const bodyParser = require('body-parser');
 //   publicPath: webpackConfig.output.publicPath,
 //   noInfo: true
 // }));
+//app.use(webpackHotMiddleware(compiler));
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use(webpackHotMiddleware(compiler));
+
 app.use(bodyParser.json({limit: '1000mb'}));
 app.use(express.static('build'));
 app.use(require('./router'));
