39e9137ec3b5d3a671f3d0ec7d94fe515ae0ba0a
Jared Murphy <jaredcmurphy@gmail.com>
2017-09-20 13:59:14 -0400

    corrects spelling of Necktie

--- client/reducers/garmentsReducer.js
+++ client/reducers/garmentsReducer.js
@@ -10,46 +10,48 @@ import {
   suitImage,
   skirtImage,
   dressImage,
-  pantsImage
+  pantsImage,
 } from '../images/garments';
 
-export default function(){
-  return {garments: [
-    {
-      id: 1,
-      title: 'Pants',
-      image: pantsImage,
-      weight: 680
-    },
-    {
-      id: 2,
-      title: 'Shirt',
-      image: shirtImage,
-      weight: 230
-    },
-    {
-      id: 3,
-      title: 'Skirt',
-      image: skirtImage,
-      weight: 340
-    },
-    {
-      id: 4,
-      title: 'Dress',
-      image: dressImage,
-      weight: 340
-    },
-    {
-      id: 5,
-      title: 'Suit Jacket',
-      image: suitImage,
-      weight: 710
-    },
-    {
-      id: 6,
-      title: 'Neck Tie',
-      image: tieImage,
-      weight: 115
-    }
-  ]}
+export default function() {
+  return {
+    garments: [
+      {
+        id: 1,
+        title: 'Pants',
+        image: pantsImage,
+        weight: 680,
+      },
+      {
+        id: 2,
+        title: 'Shirt',
+        image: shirtImage,
+        weight: 230,
+      },
+      {
+        id: 3,
+        title: 'Skirt',
+        image: skirtImage,
+        weight: 340,
+      },
+      {
+        id: 4,
+        title: 'Dress',
+        image: dressImage,
+        weight: 340,
+      },
+      {
+        id: 5,
+        title: 'Suit Jacket',
+        image: suitImage,
+        weight: 710,
+      },
+      {
+        id: 6,
+        title: 'Necktie',
+        image: tieImage,
+        weight: 115,
+      },
+    ],
+  };
 }
