818453a3cdcdc9eb0c20c96d3f51160bb691fe21
Jared Murphy <jaredcmurphy@gmail.com>
2017-09-21 18:45:34 -0400

    adds correct title for admain and tailor on arvchive view

--- client/components/orders/ArchivedOrders.js
+++ client/components/orders/ArchivedOrders.js
@@ -16,17 +16,17 @@ class ArchivedOrders extends Component {
     if (archivedOrders.length > 0) {
       return archivedOrders.map((order, i) => {
         const fulfilledDate = moment(order.fulfilled_date).format('MM-DD-YYYY');
-        let nameOrTailor, alterationsCountOrRetailer;
+        let customerOrTailor, quantityOrRetailer;
         const {id, tailor, retailer, customer, alterations_count} = order;
 
         if (tailor) {
-          nameOrTailor = tailor.name;
-          alterationsCountOrRetailer = retailer.name;
+          customerOrTailor = tailor.name;
+          quantityOrRetailer = retailer.name;
         } else {
           const {first_name, last_name} = customer;
           const name = `${first_name} ${last_name}`;
-          nameOrTailor = name;
-          alterationsCountOrRetailer = alterations_count;
+          customerOrTailor = name;
+          quantityOrRetailer = alterations_count;
         }
 
         const route = `/orders/${id}`;
@@ -38,8 +38,8 @@ class ArchivedOrders extends Component {
                 <div className="order-data" style={{color: 'green'}}>
                   {fulfilledDate}
                 </div>
-                <div className="order-data">{nameOrTailor}</div>
-                <div className="order-data">{alterationsCountOrRetailer}</div>
+                <div className="order-data">{customerOrTailor}</div>
+                <div className="order-data">{quantityOrRetailer}</div>
               </Link>
             </div>
             <hr className="order-row-hr" />
@@ -56,6 +56,15 @@ class ArchivedOrders extends Component {
       return <Redirect to="/" />;
     }
     const headerText = `Archived Orders / ${this.props.currentStore.name}`;
+    const role = this.props.currentUser.user.roles[0].name;
+    let customerOrTailor, quantityOrSource;
+    if (role === 'admin') {
+      customerOrTailor = 'Tailor';
+      quantityOrSource = 'Source';
+    } else {
+      customerOrTailor = 'Customer';
+      quantityOrSource = 'Quantity';
+    }
     return (
       <div>
         <SectionHeader text={headerText} />
@@ -63,8 +72,8 @@ class ArchivedOrders extends Component {
           <div className="order-row-header">
             <h3 className="order-column">Order</h3>
             <h3 className="order-column">FulFilled Date</h3>
-            <h3 className="order-column">Customer</h3>
-            <h3 className="order-column">Quantity</h3>
+            <h3 className="order-column">{customerOrTailor}</h3>
+            <h3 className="order-column">{quantityOrSource}</h3>
           </div>
           <hr className="order-header-hr" />
           <div className="order-rows">{this.renderOrderRows()}</div>
