951c6cadf313e676f850d1b8b80bb4e8f3295264
Jared Murphy <jaredcmurphy@gmail.com>
2017-09-22 14:11:18 -0400

    updates styles for printing instructions and shipping labels

--- client/components/prints/OrderComplete.js
+++ client/components/prints/OrderComplete.js
@@ -5,7 +5,6 @@ import isEmpty from 'lodash';
 import {renderAlterationList} from '../../utils/alterationsLists';
 
 class OrderComplete extends Component {
-
   // getAlterationsList(items){
   //   return items.map((item, index) => {
   //     return item.alterations.map((alt, index) => {
@@ -29,41 +28,56 @@ class OrderComplete extends Component {
   //   );
   // }
 
-  render(){
-      const {currentOrder, CurrentStore, shippingType} = this.props;
-      const type = shippingType === 'OutgoingShipment' ? 'outgoing_shipment' : 'incoming_shipment';
+  render() {
+    const {currentOrder, CurrentStore, shippingType} = this.props;
+    const type =
+      shippingType === 'OutgoingShipment'
+        ? 'outgoing_shipment'
+        : 'incoming_shipment';
 
-      if (currentOrder){
-        const {first_name} = currentOrder.customer;
-        const {id} = currentOrder;
-        const {shipping_label} = currentOrder[type];
-        return (
-          <div className='print'>
-            <div className='packing-slip-info'>
-              <img className='packing-slip-label' src={shipping_label} />
-              <h3>Thank you for your Air Tailor order, {first_name}</h3>
-              <p>We hope everything arrived exactly as you expected and that you are pleased with our work. If you have any questions or would like to alter/repair more clothes using Air Tailor, please text us or email hello@airtailor.com. We look forward to serving you again soon, {first_name}!</p>
-              <p className='packing-slip-info-orderid'><b>Order: #{id}</b></p>
-              {renderAlterationList(currentOrder.items, 'print-alteration')}
-              <img className='packing-slip-info-img' src={logo} alt='air tailor logo' id='logo' />
-            </div>
+    if (currentOrder) {
+      const {first_name} = currentOrder.customer;
+      const {id} = currentOrder;
+      const {shipping_label} = currentOrder[type];
+      return (
+        <div className="print">
+          <div className="packing-slip-info">
+            <img className="packing-slip-label" src={shipping_label} />
+            <br />
+            <br />
+
+            <h3>Thank you for your Air Tailor order, {first_name}</h3>
+            <p>
+              We hope everything arrived exactly as you expected and that you
+              are pleased with our work. If you have any questions or would like
+              to alter/repair more clothes using Air Tailor, please text us or
+              email hello@airtailor.com. We look forward to serving you again
+              soon, {first_name}!
+            </p>
+            <p className="packing-slip-info-orderid">
+              <b>Order: #{id}</b>
+            </p>
+            {renderAlterationList(currentOrder.items, 'print-alteration')}
+            <img
+              className="packing-slip-info-img"
+              src={logo}
+              alt="air tailor logo"
+              id="logo"
+            />
           </div>
-        );
-      } else {
-        return (
-          <div className='print'>
-            Oops something went wrong
-          </div>
-        )
-      }
+        </div>
+      );
+    } else {
+      return <div className="print">Oops something went wrong</div>;
+    }
   }
 }
 
-const mapStateToProps = (store) => {
+const mapStateToProps = store => {
   return {
     currentStore: store.currentStore,
-    currentOrder: store.currentOrder
-  }
-}
+    currentOrder: store.currentOrder,
+  };
+};
 
 export default connect(mapStateToProps)(OrderComplete);
