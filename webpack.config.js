6f6fa9e9a4cf65b4ae9ceeeb98a9f2c1e63384ac
Jared Murphy <jaredcmurphy@gmail.com>
2017-09-29 12:32:44 -0400

    add or or development to webpack.definePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,9 @@ const config = {
       template: 'client/index.html',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development'
+      ),
     }),
   ],
 };
