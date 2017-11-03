984e8490fdd4284ae4af92fca4075931e63bf00a
Jared Murphy <jaredcmurphy@gmail.com>
2017-11-02 22:55:44 -0400

    adds image case for "SuitJacket"

--- client/components/orders/show/OrdersShow.js
+++ client/components/orders/show/OrdersShow.js
@@ -136,6 +136,8 @@ class OrdersShow extends Component {
         return dressImage;
       case 'Suit Jacket':
         return suitImage;
+      case 'SuitJacket':
+        return suitImage;
       case 'Necktie':
         return tieImage;
       case 'Tie':
