25ab3186be81aeccb2f14375bba812b34b9e870f
Jared Murphy <jaredcmurphy@gmail.com>
2017-11-18 18:57:29 -0500

    fixes fulfill button for tialor

--- client/components/shipping/shippingFunctions.js
+++ client/components/shipping/shippingFunctions.js
@@ -3,79 +3,88 @@ import {
   SHIP_TAILOR_TO_RETAILER,
   SHIP_CUSTOMER_TO_TAILOR,
   SHIP_TAILOR_TO_CUSTOMER,
-  SHIP_RETAILER_TO_CUSTOMER
+  SHIP_RETAILER_TO_CUSTOMER,
 } from '../../utils/constants';
 
 import {createShipment} from '../../actions';
 
-export const fireShipmentCreate = (orders, action, type)  => {
-  const orderIds = orders.map((o) => o.id)
+export const fireShipmentCreate = (orders, action, type) => {
+  const orderIds = orders.map(o => o.id);
   return createShipment({
     shipment: {
-      delivery_type: type, order_ids: orderIds, shipment_action: action
-    }
-  })
+      delivery_type: type,
+      order_ids: orderIds,
+      shipment_action: action,
+    },
+  });
 };
 
 export const messengerAllowed = (action, roles) => {
   const {admin, retailer} = roles;
 
-  switch(action) {
+  switch (action) {
     case SHIP_RETAILER_TO_TAILOR:
       if (admin || retailer) {
-        return true
+        return true;
       } else {
-        return false
+        return false;
       }
     default:
-      return false
+      return false;
   }
 };
 
 export const getShipmentForRole = (roles, order) => {
   const {shipments} = order;
-  if (roles.admin && order.type === "WelcomeKit") {
-    return shipments.find( (s) => {
-      return s.source.address_type      === "retailer" &&
-             s.destination.address_type === "customer"
-    })
+  if (roles.admin && order.type === 'WelcomeKit') {
+    return shipments.find(s => {
+      return (
+        s.source.address_type === 'retailer' &&
+        s.destination.address_type === 'customer'
+      );
+    });
   } else if (roles.tailor || roles.admin) {
     if (order.ship_to_store) {
-      return shipments.find( (s) => {
-        return s.destination.address_type === "retailer" &&
-               s.source.address_type      ===  "tailor"
-      })
+      return shipments.find(s => {
+        return (
+          s.destination.address_type === 'retailer' &&
+          s.source.address_type === 'tailor'
+        );
+      });
     } else {
-      return shipments.find( (s) => {
-        return s.destination.address_type === "customer" &&
-               s.source.address_type      ===  "tailor"
-      })
+      return shipments.find(s => {
+        return (
+          s.destination.address_type === 'customer' &&
+          s.source.address_type === 'tailor'
+        );
+      });
     }
   } else if (roles.retailer) {
-    return shipments.find( (s) => {
-      return s.destination.address_type === "source" &&
-             s.source.address_type      ===  "retailer"
-    })
+    return shipments.find(s => {
+      return (
+        s.destination.address_type === 'source' &&
+        s.source.address_type === 'retailer'
+      );
+    });
   }
 };
 
 export const correctShipmentExists = (roles, order) => {
   const {shipments} = order;
   if (!shipments || shipments.length == 0) return false;
-  const {source, destination} = getShipmentForRole(roles, order)
-  return (source && destination);
+  const correctShipment = getShipmentForRole(roles, order);
+  return correctShipment; // either a shipment, or undefined
 };
-
 
 export const labelState = (roles, order, loadingLabel) => {
   const shipmentExists = correctShipmentExists(roles, order);
   if (!shipmentExists) {
-    return 'needs_label'
+    return 'needs_label';
   } else {
     if (loadingLabel) {
-      return 'in_progress'
+      return 'in_progress';
     } else {
-      return 'label_created'
+      return 'label_created';
     }
   }
 };
@@ -83,46 +92,46 @@ export const labelState = (roles, order, loadingLabel) => {
 export const messengerState = (roles, order, sendingMessenger) => {
   const shipmentExists = correctShipmentExists(roles, order);
   if (shipmentExists) {
-    return 'needs_delivery'
+    return 'needs_delivery';
   } else {
     if (sendingMessenger) {
-      return 'in_progress'
+      return 'in_progress';
     } else {
-      return 'package_delivered'
+      return 'package_delivered';
     }
   }
 };
 
-export const shipmentTypes = (roles) => {
-  const {retailer, tailor, admin, customer} = roles
-  const allShipmentTypes = new Set(['mail_shipment', 'messenger_shipment'])
+export const shipmentTypes = roles => {
+  const {retailer, tailor, admin, customer} = roles;
+  const allShipmentTypes = new Set(['mail_shipment', 'messenger_shipment']);
 
   if (admin || retailer) {
-    return allShipmentTypes
+    return allShipmentTypes;
   } else if (tailor) {
-    allShipmentTypes.delete('messenger_shipment')
+    allShipmentTypes.delete('messenger_shipment');
   } else if (customer) {
-    allShipmentTypes.clear()
+    allShipmentTypes.clear();
   }
 
-  return allShipmentTypes
-}
+  return allShipmentTypes;
+};
 
 export const shipmentActions = (order, roles) => {
   const {ship_to_store, type} = order;
-  const {retailer, tailor, admin, customer} = roles
+  const {retailer, tailor, admin, customer} = roles;
 
-  if (ship_to_store && tailor){
+  if (ship_to_store && tailor) {
     return SHIP_TAILOR_TO_RETAILER;
-  } else if (!ship_to_store && tailor){
+  } else if (!ship_to_store && tailor) {
     return SHIP_TAILOR_TO_CUSTOMER;
-  } else if (retailer && type == "TailorOrder"){
+  } else if (retailer && type == 'TailorOrder') {
     return SHIP_RETAILER_TO_TAILOR;
-  } else if (admin){
-    if (type == "WelcomeKit") {
+  } else if (admin) {
+    if (type == 'WelcomeKit') {
       return SHIP_RETAILER_TO_CUSTOMER;
-    } else if (type == "TailorOrder") {
+    } else if (type == 'TailorOrder') {
       return SHIP_RETAILER_TO_TAILOR;
-    };
-  };
+    }
+  }
 };
