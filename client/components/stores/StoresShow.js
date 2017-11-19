08b13bb0eaf76c6b62e51b1aba2f12d6e9010752
Jared Murphy <jaredcmurphy@gmail.com>
2017-11-18 19:11:41 -0500

    fixes error - TypeError: orderHeader is not a function - line 404

--- client/components/stores/StoresShow.js
+++ client/components/stores/StoresShow.js
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import moment from "moment";
-import { Redirect, Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import moment from 'moment';
+import {Redirect, Link} from 'react-router-dom';
 import {
   getStoreOrders,
   setLoader,
   removeLoader,
   alertCustomersPickup,
-  setGrowler
-} from "../../actions";
-import SectionHeader from "../SectionHeader";
-import isEmpty from "lodash/isEmpty";
-import Checkbox from "../Checkbox";
+  setGrowler,
+} from '../../actions';
+import SectionHeader from '../SectionHeader';
+import isEmpty from 'lodash/isEmpty';
+import Checkbox from '../Checkbox';
 
 import {
   shipmentTypes,
   shipmentActions,
   labelState,
   messengerAllowed,
-  fireShipmentCreate
-} from "../shipping/shippingFunctions";
+  fireShipmentCreate,
+} from '../shipping/shippingFunctions';
 
 class StoresShow extends Component {
   // NOTE: still needs A) count of ordrs in the title, and B) a loooot of styling
   constructor(props) {
     super();
     this.state = {
-      showOrderState: "new_orders",
-      selectedOrders: new Set()
+      showOrderState: 'new_orders',
+      selectedOrders: new Set(),
     };
 
     this.toggleOrderSelect = this.toggleOrderSelect.bind(this);
@@ -46,20 +46,20 @@ class StoresShow extends Component {
   }
 
   componentDidMount() {
-    const { currentUser: { store_id: storeId } } = this.props;
+    const {currentUser: {store_id: storeId}} = this.props;
     this.refreshStoreOrders();
   }
 
   refreshStoreOrders() {
     this.props.setLoader();
-    const { currentUser: { store_id: storeId } } = this.props;
-    const { getStoreOrders } = this.props;
+    const {currentUser: {store_id: storeId}} = this.props;
+    const {getStoreOrders} = this.props;
 
-    this.setState({ loadingOrders: true });
+    this.setState({loadingOrders: true});
     getStoreOrders(storeId)
       .then(() => {
         this.state.selectedOrders = new Set();
-        this.setState({ loadingOrders: false });
+        this.setState({loadingOrders: false});
         this.props.removeLoader();
       })
       .catch(err => console.log(err));
@@ -68,16 +68,16 @@ class StoresShow extends Component {
   formatStatusString(dueDate, late) {
     const todaysDate = moment(new Date());
     const momentDueDate = moment(dueDate);
-    const diff = Math.abs(momentDueDate.diff(todaysDate, "days"));
-    const additionalString = late ? " days late" : " days to go";
+    const diff = Math.abs(momentDueDate.diff(todaysDate, 'days'));
+    const additionalString = late ? ' days late' : ' days to go';
     const status = (diff + additionalString).toUpperCase();
     return status;
   }
 
   sortOrdersByStatus(status) {
-    const { openOrders: orders, userRoles: roles } = this.props;
+    const {openOrders: orders, userRoles: roles} = this.props;
     switch (status) {
-      case "new_orders":
+      case 'new_orders':
         if (roles.tailor) {
           // this is where i'd like shipped to exist.
           return orders.filter(
@@ -86,7 +86,7 @@ class StoresShow extends Component {
         } else {
           return orders.filter(order => isEmpty(order.shipments));
         }
-      case "in_progress_orders":
+      case 'in_progress_orders':
         if (roles.tailor) {
           return orders.filter(order => order.arrived && !order.fulfilled);
         } else {
@@ -95,9 +95,9 @@ class StoresShow extends Component {
               !isEmpty(order.shipments) && order.tailor && !order.fulfilled
           );
         }
-      case "ready_orders":
+      case 'ready_orders':
         return orders.filter(order => order.fulfilled);
-      case "late_orders":
+      case 'late_orders':
         return orders.filter(order => order.late);
       default:
         return orders;
@@ -116,33 +116,33 @@ class StoresShow extends Component {
       due_date,
       fulfilled,
       customer_alerted,
-      ship_to_store
+      ship_to_store,
     } = order;
 
     let status, color;
 
     if (isEmpty(order.shipments)) {
-      status = "Needs Shipping Details";
-      color = "gold";
+      status = 'Needs Shipping Details';
+      color = 'gold';
     } else if (!isEmpty(order.shipments) && !order.arrived) {
-      status = "In Transit";
-      color = "green";
+      status = 'In Transit';
+      color = 'green';
     } else if (order.late) {
       let dueTime = this.formatStatusString(order.due_date, true);
       status = dueTime;
-      color = "red";
+      color = 'red';
     } else if (
       order.fulfilled &&
       !order.customer_alerted &&
       order.ship_to_store
     ) {
-      status = "Ready for Customer";
-      color: "green";
+      status = 'Ready for Customer';
+      color: 'green';
     } else if (order.arrived && !order.fulfilled) {
       status = this.formatStatusString(order.due_date, false);
-      color = "orange";
+      color = 'orange';
     }
-    return { status, color };
+    return {status, color};
   }
 
   postShipment(orders, action, type) {
@@ -150,48 +150,48 @@ class StoresShow extends Component {
     fireShipmentCreate(orders, action, type)
       .then(res => {
         this.props.removeLoader();
-        this.setState({ loadingLabel: false });
+        this.setState({loadingLabel: false});
         this.refreshStoreOrders();
       })
-      .catch(err => console.log("err", err));
+      .catch(err => console.log('err', err));
   }
 
   // NOTE: these should go in shippingFunctions.js
   makeLabels([...orders]) {
     // This should [...print once with X separate labels (x == number of orders])
-    const { userRoles: roles } = this.props;
+    const {userRoles: roles} = this.props;
     const order = [...orders][0];
     const action = shipmentActions(order, roles);
-    return this.postShipment(orders, action, "mail_shipment");
+    return this.postShipment(orders, action, 'mail_shipment');
   }
 
   sendMessenger(orders) {
     // everything passed here
-    const { userRoles: roles } = this.props;
+    const {userRoles: roles} = this.props;
     const order = orders[0];
     const action = shipmentActions(order, roles);
-    console.log("POSTING MESSENGER", orders);
-    return this.postShipment(orders, action, "messenger_shipment");
+    console.log('POSTING MESSENGER', orders);
+    return this.postShipment(orders, action, 'messenger_shipment');
   }
 
   toggleOrderSelect(order) {
     if (!this.state.selectedOrders.has(order)) {
       const newSelectedOrders = this.state.selectedOrders;
       newSelectedOrders.add(order);
-      this.setState({ seletecdOrders: newSelectedOrders });
+      this.setState({seletecdOrders: newSelectedOrders});
     } else {
       const newSelectedOrders = this.state.selectedOrders;
       newSelectedOrders.delete(order);
-      this.setState({ seletecdOrders: newSelectedOrders });
+      this.setState({seletecdOrders: newSelectedOrders});
     }
   }
 
   setOrderTabState(state) {
-    this.setState({ showOrderState: state });
+    this.setState({showOrderState: state});
   }
 
   renderShippingControls() {
-    const { userRoles: roles } = this.props;
+    const {userRoles: roles} = this.props;
     if (roles.admin || roles.retailer) {
       const orders = this.state.selectedOrders;
       const labelFunction = () => this.makeLabels([...orders]);
@@ -223,7 +223,7 @@ class StoresShow extends Component {
   }
 
   renderAlertCustomers() {
-    const { userRoles: roles, currentStore: { id: store_id } } = this.props;
+    const {userRoles: roles, currentStore: {id: store_id}} = this.props;
     if (roles.admin || roles.retailer) {
       const orders = this.state.selectedOrders;
 
@@ -234,10 +234,10 @@ class StoresShow extends Component {
         alertCustomersPickup(orders, store_id).then(res => {
           this.props.removeLoader();
           if (res.body.status === 200) {
-            const kind = "success";
+            const kind = 'success';
             const message =
-              "Your customers have been notified to pick up their orders";
-            this.props.setGrowler({ kind, message });
+              'Your customers have been notified to pick up their orders';
+            this.props.setGrowler({kind, message});
             this.refreshStoreOrders();
           }
         });
@@ -263,15 +263,15 @@ class StoresShow extends Component {
 
   renderOrderRow(order) {
     const orderStatus = this.getOrderStatus(order);
-    const { id, customer, alterations_count } = order;
-    const { first_name, last_name } = customer;
-    const { color, status } = orderStatus;
+    const {id, customer, alterations_count} = order;
+    const {first_name, last_name} = customer;
+    const {color, status} = orderStatus;
     const route = `/orders/${id}`;
     return (
       <div className="order-row" key={id}>
         <Link to={route} className="order-row-link-no-select">
           <div className="order-cell-no-select">#{id}</div>
-          <div style={{ color }} className="order-cell-no-select">
+          <div style={{color}} className="order-cell-no-select">
             {status}
           </div>
           <div className="order-cell-no-select">
@@ -285,15 +285,15 @@ class StoresShow extends Component {
   }
 
   renderOrderRowWithSelect(order) {
-    const { userRoles: roles } = this.props;
-    const { id, customer, tailor, alterations_count } = order;
-    const { first_name, last_name } = customer;
+    const {userRoles: roles} = this.props;
+    const {id, customer, tailor, alterations_count} = order;
+    const {first_name, last_name} = customer;
     let tailorDiv = <div />;
 
     if (tailor && (roles.admin || roles.retailer)) {
       tailorDiv = <div className="order-data-cell">{tailor.name}</div>;
     }
-    const { color, status } = this.getOrderStatus(order);
+    const {color, status} = this.getOrderStatus(order);
     const route = `/orders/${id}`;
 
     let orderSelect = <div />;
@@ -313,7 +313,7 @@ class StoresShow extends Component {
         <div className="order-select-cell">{orderSelect}</div>
         <Link to={route} className="order-row-link">
           <div className="order-data-cell">#{id}</div>
-          <div style={{ color }} className="order-data-cell">
+          <div style={{color}} className="order-data-cell">
             {status}
           </div>
           <div className="order-data-cell">
@@ -329,27 +329,27 @@ class StoresShow extends Component {
 
   renderStateTabs() {
     const allTabs = [
-      { className: "order-state-tab", status: "new_orders", text: "New" },
+      {className: 'order-state-tab', status: 'new_orders', text: 'New'},
       {
-        className: "order-state-tab",
-        status: "in_progress_orders",
-        text: "Current"
+        className: 'order-state-tab',
+        status: 'in_progress_orders',
+        text: 'Current',
       },
       {
-        className: "order-state-tab",
-        status: "ready_orders",
-        text: "Finished"
+        className: 'order-state-tab',
+        status: 'ready_orders',
+        text: 'Finished',
       },
-      { className: "order-state-tab", status: "late_orders", text: "Late" }
+      {className: 'order-state-tab', status: 'late_orders', text: 'Late'},
     ];
 
     const tabs = allTabs.map((tab, i) => {
       if (tab.status == this.state.showOrderState) {
-        tab.className = tab.className.concat(" selected");
+        tab.className = tab.className.concat(' selected');
       }
-      if (tab.status == "late_orders") {
+      if (tab.status == 'late_orders') {
         if (this.countOrdersByStatus(tab.status) > 0) {
-          tab.className = tab.className.concat(" late-orders");
+          tab.className = tab.className.concat(' late-orders');
         }
       }
 
@@ -386,10 +386,10 @@ class StoresShow extends Component {
       <div className="order-headers-container">
         <div className="order-headers-row-no-select">
           <div className="order-headers-container-no-select">
-            {orderHeader("Id", false)}
-            {orderHeader("Status", false)}
-            {orderHeader("Customer", false)}
-            {orderHeader("Quantity", false)}
+            {orderHeader('Id', false)}
+            {orderHeader('Status', false)}
+            {orderHeader('Customer', false)}
+            {orderHeader('Quantity', false)}
           </div>
         </div>
       </div>
@@ -397,17 +397,17 @@ class StoresShow extends Component {
   }
 
   renderRetailerHeaders() {
-    const orderHeader = this.renderOrderHeader;
+    const orderHeader = this.renderHeaderCell;
     return (
       <div className="order-headers-container">
         <div className="order-headers-row">
-          {orderHeader("Select:", false, true)}
+          {orderHeader('Select:', false, true)}
           <div className="order-data-headers-container">
-            {orderHeader("Order", true, false)}
-            {orderHeader("Status", true, false)}
-            {orderHeader("Customer", true, false)}
-            {orderHeader("Tailor", true, false)}
-            {orderHeader("Quantity", true, false)}
+            {orderHeader('Order', true, false)}
+            {orderHeader('Status', true, false)}
+            {orderHeader('Customer', true, false)}
+            {orderHeader('Tailor', true, false)}
+            {orderHeader('Quantity', true, false)}
           </div>
         </div>
       </div>
@@ -415,7 +415,7 @@ class StoresShow extends Component {
   }
 
   renderRetailerRows() {
-    const { openOrders } = this.props;
+    const {openOrders} = this.props;
     if (!isEmpty(openOrders)) {
       const status = this.state.showOrderState;
       const sortedOrders = this.sortOrdersByStatus(status);
@@ -440,8 +440,8 @@ class StoresShow extends Component {
   }
 
   renderTailorRows() {
-    const { openOrders } = this.props;
-    const ordersWithShipments = this.sortOrdersByStatus("new_orders");
+    const {openOrders} = this.props;
+    const ordersWithShipments = this.sortOrdersByStatus('new_orders');
 
     if (!isEmpty(ordersWithShipments)) {
       const status = this.state.showOrderState;
@@ -470,7 +470,7 @@ class StoresShow extends Component {
       return <Redirect to="/" />;
     }
 
-    const { userRoles: { tailor, retailer, admin } } = this.props;
+    const {userRoles: {tailor, retailer, admin}} = this.props;
     const headerText = `Orders / ${this.props.currentStore.name}`;
 
     if (retailer || admin) {
@@ -514,7 +514,7 @@ const mapStateToProps = store => {
     currentUser: store.currentUser,
     currentStore: store.currentStore,
     openOrders: store.storeOrders,
-    userRoles: store.userRoles
+    userRoles: store.userRoles,
   };
 };
 
@@ -524,7 +524,7 @@ const mapDispatchToProps = dispatch => {
       getStoreOrders,
       setLoader,
       removeLoader,
-      setGrowler
+      setGrowler,
     },
     dispatch
   );
