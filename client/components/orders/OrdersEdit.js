4b6ce80fd0b137615d9b5cf5ffb4f9b9e7df3920
nialbima@gmail.com <nialbima@gmail.com>
2017-11-25 16:18:57 -0500

    refactored, and added notifications to postmates

--- client/components/orders/OrdersEdit.js
+++ client/components/orders/OrdersEdit.js
@@ -1,34 +1,52 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import FormSelect from '../FormSelect';
 import FormField from '../FormField';
-import {updateOrder, getTailorList} from '../../actions';
+import { updateOrder, getTailorList } from '../../actions';
 import SelectTailor from './orderForms/SelectTailor';
+import PropTypes from 'prop-types';
+
+const mapStateToProps = store => {
+  return {
+    order: store.currentOrder,
+    tailors: store.tailorList
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ getTailorList, updateOrder }, dispatch);
+};
 
 class OrdersEdit extends Component {
+  static propTypes = {
+    order: PropTypes.object.isRequired, // mapStateToProps
+    tailors: PropTypes.array.isRequired, // mapStateToProps
+    getTailorList: PropTypes.func.isRequired, // mapDispatchToProps
+    updateOrder: PropTypes.func.isRequired // mapDispatchToProps
+  };
+
   constructor(props) {
     super();
     this.state = props.order;
-    this.updateState = this.updateState.bind(this);
   }
 
-  updateState(field, value) {
-    this.setState({[field]: value});
-  }
+  updateState = (field, value) => {
+    this.setState({ [field]: value });
+  };
 
   handleSubmit(e) {
     e.preventDefault();
     this.props
-      .updateOrder({order: this.state})
+      .updateOrder({ order: this.state })
       // .then(res => console.log('res', res))
       .catch(err => console.log('errr', err));
   }
 
   render() {
-    const {customer, total, weight, provider_id} = this.state;
-    const {first_name, last_name} = customer;
+    const { customer, total, weight, provider_id } = this.state;
+    const { first_name, last_name } = customer;
     const customerName = first_name + ' ' + last_name;
     const backLink = `/orders/${this.state.id}`;
 
@@ -80,16 +98,5 @@ class OrdersEdit extends Component {
     }
   }
 }
-
-const mapStateToProps = store => {
-  return {
-    order: store.currentOrder,
-    tailors: store.tailorList,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({getTailorList, updateOrder}, dispatch);
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(OrdersEdit);
