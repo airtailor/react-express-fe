cc1a2a6fbd17a74049fa07e632acd2f476e15d2e
Jared Murphy <jaredcmurphy@gmail.com>
2017-11-29 18:33:48 -0500

    adds error handling and success handling for react token creation for stripe

--- client/components/stores/StripeDefaultPaymentForm.js
+++ client/components/stores/StripeDefaultPaymentForm.js
@@ -1,22 +1,75 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { injectStripe, CardElement } from 'react-stripe-elements';
+import { setGrowler, setLoader, removeLoader } from '../../actions';
+
+const mapStateToProps = store => {
+  return {
+    currentStore: store.currentStore,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ setGrowler, setLoader, removeLoader }, dispatch);
+};
 
 class StripeDefaultPaymentForm extends Component {
   static propTypes = {
+    currentStore: PropTypes.object.isRequired, // mapStateToProps
+    removeLoader: PropTypes.func.isRequired, // mapDispatchToProps
+    setLoader: PropTypes.func.isRequired, // mapDispatchToProps
+    setGrowler: PropTypes.func.isRequired, // mapDispatchToProps
     stripe: PropTypes.shape({
       createToken: PropTypes.func.isRequired,
       _apiKey: PropTypes.string.isRequired,
     }), // injectStripe HOC
   };
 
-  handleSubmit(e) {
+  handleSuccess(token) {
+    const { removeLoader, setGrowler } = this.props;
+
+    removeLoader();
+    const kind = 'success';
+    const message = 'Default Payment Updated';
+    setGrowler({ kind, message });
+    console.log('Received Stripe token:', token);
+  }
+
+  handleError(err) {
+    const {
+      removeLoader,
+      setGrowler,
+      currentStore: { name: storeName },
+    } = this.props;
+
+    removeLoader();
+    const kind = 'warning';
+    const message = 'Oops Something Went Wrong';
+    setGrowler({ kind, message });
+    console.log(err);
+  }
+
+  handleSubmit = e => {
     e.preventDefault();
 
-    this.props.stripe.createToken({ name: 'Jenny Rosen' }).then(({ token }) => {
-      console.log('Received Stripe token:', token);
-    });
-  }
+    const {
+      stripe: { createToken },
+      setLoader,
+      currentStore: { name: storeName },
+    } = this.props;
+    setLoader();
+
+    // we should be passing in the store's stripe id once that is a thing
+    createToken({ name: storeName })
+      .then(({ token }) => {
+        this.handleSuccess(token);
+      })
+      .catch(err => {
+        this.handleError(err);
+      });
+  };
 
   createOptions() {
     return {
@@ -38,7 +91,6 @@ class StripeDefaultPaymentForm extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -51,4 +103,6 @@ class StripeDefaultPaymentForm extends Component {
   }
 }
 
-export default injectStripe(StripeDefaultPaymentForm);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  injectStripe(StripeDefaultPaymentForm)
+);
