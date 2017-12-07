3f67a5ca9e09a5ba916aaf56456e165596497a51
nialbima@gmail.com <nialbima@gmail.com>
2017-12-07 15:13:48 -0500

    okay! row renderings are done. 	the Edit User pane needs a little more detail, it looks weird rn.

--- client/components/users/list/ducks/usersListReducer.js
+++ client/components/users/list/ducks/usersListReducer.js
@@ -1,9 +1,7 @@
 import { SET_USERS_LIST } from './constants';
 
 const initialState = [];
-
 const usersListReducer = (state = initialState, action) => {
-  console.log(action);
   switch (action.type) {
     case SET_USERS_LIST:
       return action.users;
--- client/components/users/list/index.js
+++ client/components/users/list/index.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { setLoader, removeLoader, getUsersList } from './ducks/actions';
 import SectionHeader from '../../SectionHeader';
 import { ValidatePassword } from '../../../utils/validations';
+import isEmpty from 'lodash/isEmpty';
 
 const mapStateToProps = store => {
   return {
@@ -21,22 +22,66 @@ const mapDispatchToProps = dispatch => {
 
 class UsersList extends Component {
   componentDidMount() {
-    const { setLoader, removeLoader, getUsersList } = this.props;
-    setLoader();
-    getUsersList().then(res => {
-      removeLoader();
-    });
+    const { users } = this.props;
+    if (isEmpty(users)) {
+      const { setLoader, removeLoader, getUsersList } = this.props;
+      setLoader();
+      getUsersList().then(res => removeLoader());
+    }
   }
 
-  render() {
-    const { users } = this.props;
-    console.log('USERS', users);
+  renderUserRow = user => {
+    const { id, email, store_id } = user;
+    const route = `/users/${id}/edit`;
+    return (
+      <div key={id}>
+        <div className="user-data-row">
+          <Link to={route} className="user-link">
+            <div className="user-data-cell">{email}</div>
+          </Link>
+        </div>
+      </div>
+    );
+  };
 
+  renderUserRows = () => {
+    const { users } = this.props;
+    if (!isEmpty(users)) {
+      const userRowSet = users.map(user => this.renderUserRow(user));
+      return <div className="users-container">{userRowSet}</div>;
+    } else {
+      return (
+        <div className="table-row">
+          <div className="loading-orders">Loading Users...</div>
+        </div>
+      );
+    }
+  };
+
+  renderUserHeaders = () => {
+    return (
+      <div>
+        <div className="user-headers-container">
+          <div className="user-headers-row">
+            <h3 className="user-header-cell">User</h3>
+          </div>
+          <hr className="user-header-break-row" />
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    const userHeaders = this.renderUserHeaders;
+    const userRows = this.renderUserRows;
     return (
       <div>
         // pass in users/new to the link
-        <SectionHeader />
-        // table for users // links to individual users to edit them.
+        <SectionHeader text={'Manage Users'} />
+        <div className="users">
+          {userHeaders()}
+          {userRows()}
+        </div>
       </div>
     );
   }
