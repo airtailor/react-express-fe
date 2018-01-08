073b5b37ca47a58f13cad6ff3e66814b19cea5f1
Jared Murphy <jaredcmurphy@gmail.com>
2018-01-08 13:09:32 -0500

    adds new measurements images

--- client/images/measurements/index.js
+++ client/images/measurements/index.js
@@ -1,2 +1,4 @@
-export const FrontImage = 'https://i.imgur.com/5w6R51N.png';
-export const BackImage = 'https://i.imgur.com/BMLPKyi.png';
+export const FrontImage =
+  'https://s3.us-east-2.amazonaws.com/airtailor-images/new_measurments_front.png';
+export const BackImage =
+  'https://s3.us-east-2.amazonaws.com/airtailor-images/new_measurements_back.png';
