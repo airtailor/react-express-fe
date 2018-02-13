0d6f6ddbb8c4cb71a2046171ffc89ef0d6c04291
Jared Murphy <jaredcmurphy@gmail.com>
2018-02-13 11:36:16 -0500

    reformat address

--- client/components/orders/show/DeliveryDetails.js
+++ client/components/orders/show/DeliveryDetails.js
@@ -3,26 +3,22 @@ import OrderShowTitle from './OrderShowTitle';
 
 class DeliveryDetails extends Component {
   render() {
-    const deliveryText = this.props.order.ship_to_store
+    const { ship_to_store, retailer, customer } = this.props.order;
+
+    const address = ship_to_store ? retailer : customer;
+    const deliveryText = ship_to_store
       ? 'DELIVERY TO STORE'
       : 'DELIVERY TO CUSTOMER';
-
-    const deliveryAddress = this.props.order.ship_to_store
-      ? this.props.order.retailer
-      : this.props.order.customer;
-
-    console.log('deliveryAddress', deliveryAddress);
 
     return (
       <div>
         <OrderShowTitle title={deliveryText} />
         <div style={{ paddingLeft: '15px', fontSize: '14px' }}>
-          <p>{deliveryAddress.name}</p>
-          <p>{deliveryAddress.street1}</p>
-          <p>{deliveryAddress.street2}</p>
+          <p>{address.name || `${address.first_name} ${address.last_name}`}</p>
+          <p>{address.street1}</p>
+          <p>{address.street2}</p>
           <p>
-            {deliveryAddress.city}, {deliveryAddress.state}{' '}
-            {deliveryAddress.zip}
+            {address.city}, {address.state} {address.zip}
           </p>
         </div>
       </div>
