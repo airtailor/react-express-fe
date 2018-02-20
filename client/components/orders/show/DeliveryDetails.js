80d825b53c9240af6fd6bf505c979c550dcbb233
Jared Murphy <jaredcmurphy@gmail.com>
2018-02-20 12:19:37 -0500

    fixes customer address field on order show page

--- client/components/orders/show/DeliveryDetails.js
+++ client/components/orders/show/DeliveryDetails.js
@@ -2,10 +2,61 @@ import React, { Component } from 'react';
 import OrderShowTitle from './OrderShowTitle';
 
 class DeliveryDetails extends Component {
+  renderOldAddress(address){
+    return (
+      <div className="order-show-p-content">
+        <p>{address.name || `${address.first_name} ${address.last_name}`}</p>
+        <p>{address.street1}</p>
+        <p>{address.street2}</p>
+        <p>
+          {address.city}, {address.state} {address.zip}
+        </p>
+      </div>
+    );
+  }
+
+  renderNewAddress(address){
+    return (
+      <div className="order-show-p-content">
+        <p>{address.name}</p>
+        <p>{address.street}</p>
+        <p>{address.street_two}</p>
+        <p>
+          {address.city}, {address.state_province} {address.zip_code}
+        </p>
+      </div>
+    );
+  }
+
+
+  determineAddress(){
+    const { ship_to_store, retailer, customer } = this.props.order;
+    if (ship_to_store) {
+      return retailer.address || retailer;
+    } else {
+      return customer.addresses[0] || customer;
+    }
+  }
+
+  determineAddressName(){
+    const { ship_to_store, retailer, customer } = this.props.order;
+    if (ship_to_store){
+      return retailer.name;
+    } else {
+      return `${customer.first_name} ${customer.last_name}`;
+    }
+  }
+
   render() {
     const { ship_to_store, retailer, customer } = this.props.order;
 
-    const address = ship_to_store ? retailer : customer;
+    const address = this.determineAddress();
+    address.name = this.determineAddressName();
+
+    const displayAddress = address.street ? 
+      this.renderNewAddress(address) : 
+      this.renderOldAddress(address);
+
     const deliveryText = ship_to_store
       ? 'DELIVERY TO STORE'
       : 'DELIVERY TO CUSTOMER';
@@ -13,14 +64,7 @@ class DeliveryDetails extends Component {
     return (
       <div>
         <OrderShowTitle title={deliveryText} />
-        <div className="order-show-p-content">
-          <p>{address.name || `${address.first_name} ${address.last_name}`}</p>
-          <p>{address.street1}</p>
-          <p>{address.street2}</p>
-          <p>
-            {address.city}, {address.state} {address.zip}
-          </p>
-        </div>
+        { displayAddress }
       </div>
     );
   }
