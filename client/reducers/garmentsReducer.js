a4209358b2aaf7ae6783afbfc2782df3a62077e2
Jared Murphy <jaredcmurphy@gmail.com>
2018-03-05 18:20:48 -0500

    updates pants to 680

--- client/reducers/garmentsReducer.js
+++ client/reducers/garmentsReducer.js
@@ -1,48 +1,41 @@
 export default function() {
-  return {
-    garments: [
-      {
-        id: 1,
-        title: 'Pants',
-        image:
-          'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/pants.png',
-        weight: 450,
-      },
-      {
-        id: 2,
-        title: 'Shirt',
-        image:
-          'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/shirt.png',
-        weight: 225,
-      },
-      {
-        id: 3,
-        title: 'Skirt',
-        image:
-          'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/skirt.png',
-        weight: 340,
-      },
-      {
-        id: 4,
-        title: 'Dress',
-        image:
-          'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/dress.png',
-        weight: 340,
-      },
-      {
-        id: 5,
-        title: 'Suit Jacket',
-        image:
-          'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/jacket.png',
-        weight: 710,
-      },
-      {
-        id: 6,
-        title: 'Coat/Jacket',
-        image:
-          'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/coat_jacket.png',
-        weight: 710,
-      },
-    ],
-  };
+    return {
+        garments: [{
+                id: 1,
+                title: 'Pants',
+                image: 'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/pants.png',
+                weight: 680,
+            },
+            {
+                id: 2,
+                title: 'Shirt',
+                image: 'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/shirt.png',
+                weight: 225,
+            },
+            {
+                id: 3,
+                title: 'Skirt',
+                image: 'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/skirt.png',
+                weight: 340,
+            },
+            {
+                id: 4,
+                title: 'Dress',
+                image: 'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/dress.png',
+                weight: 340,
+            },
+            {
+                id: 5,
+                title: 'Suit Jacket',
+                image: 'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/jacket.png',
+                weight: 710,
+            },
+            {
+                id: 6,
+                title: 'Coat/Jacket',
+                image: 'https://s3.us-east-2.amazonaws.com/airtailor-images/garments/coat_jacket.png',
+                weight: 710,
+            },
+        ],
+    };
 }
